d71202278fe4038099a0d2469e8adbc170300551
Prateekbangwal <prateekbangwal22@gmail.com>
2023-03-26 14:45:54 +0530

    initial model building commit

---
+++ model_building.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 26 11:46:47 2023
+
+@author: prateekbangwal
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('data_eda.csv')
